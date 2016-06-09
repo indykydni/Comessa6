af79bf0959aee8395cfa4e87f1150063e666db6a
michalguzek <michal.guzek@gmail.com>
2016-06-09 22:02:43 +0200

    playing with db and bootstrap

--- Comessa6/Comessa6/Controllers/HomeController.cs
+++ Comessa6/Comessa6/Controllers/HomeController.cs
@@ -13,22 +13,29 @@ namespace Comessa6.Controllers
     [Authorize]
     public ActionResult Index()
     {
+      if (Session["UserID"] == null)
+      {
+        //System.Web.Security.FormsAuthentication.GetAuthCookie()
+        return RedirectToAction("Login", "Authentication");
+      }
       OrdersViewModel orders = new OrdersViewModel();
       using (var db = new comessa5Entities())
       {
-        var s = db.corder.Include("citem");//.Include("cprovider");nestedinclude
-    //.Where(corder => corder.userId == (int)Session["UserID"]);
-        var ss = (from corder order in db.corder
-                  where order.userId.Equals(Session["UserID"])
-                  select new OrderViewModel
-                  {
-                    ItemName = order.itemName,
-                    Price = order.price,
-                    Comment = order.comment,
-                    Quantity = order.quantity,
-                    UserName = Session["UserName"].ToString()//,
-                                                  //ProviderName = from citem item in db.citem where item.id == order.itemId select item.
-                  });
+        int id = (int)Session["UserID"];
+        var s = db.corder.Include("citem").Include("citem.cprovider")
+          .Where(order => order.userId == id).ToList();
+        //var ss = (from corder order in db.corder
+        //          where order.userId.Equals(Session["UserID"])
+        //          select new OrderViewModel
+        //          {
+        //            ItemName = order.itemName,
+        //            Price = order.price,
+        //            Comment = order.comment,
+        //            Quantity = order.quantity,
+        //            UserName = Session["UserName"].ToString()//,
+        //                                          //ProviderName = from citem item in db.citem where item.id == order.itemId select item.
+        //          });
+        
       }
       return View();
     }
