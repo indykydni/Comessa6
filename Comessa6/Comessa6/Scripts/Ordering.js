a5dfa7f3742f7b0a6ce6805917003606de38da63
michalguzek <michal.guzek@gmail.com>
2016-06-20 21:48:36 +0200

    ajax vs items

--- Comessa6/Comessa6/Scripts/Ordering.js
+++ Comessa6/Comessa6/Scripts/Ordering.js
@@ -4,15 +4,3 @@
     orderItem.setAttribute("itemID", button.id);
     document.getElementById("orderQuantity").value = 1;    
 }
-
-
-function ProviderClicked(providerID) {    
-    var xhttp;
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            document.getElementById("provider" + providerID).innerHTML = "dupa"
-        }
-    }
-    //var itemsPanel = document.getElementById("provider" + providerID);
-}
