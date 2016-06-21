d5d9c9c34bc5513395078abbcb19bdbfcd6c3d9e
GUZEK Michal - COMPDATA <michal.guzek@compdata.eu>
2016-06-21 13:34:07 +0200

    ajax dynamic async loading of items

--- Comessa6/Comessa6/Controllers/ItemsController.cs
+++ Comessa6/Comessa6/Controllers/ItemsController.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Comessa6.Models;
 using Comessa6.ViewModels;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
 
 namespace Comessa6.Controllers
 {
   public class ItemsController : Controller
   {
     // GET: Items
-    public ActionResult GetItems(int providerID)
+    [HttpGet]
+    public async Task<ActionResult> GetItems(int providerID)
     {
-      List<ItemViewModel> items = new List<ItemViewModel>();
-
-      return PartialView("ItemsView", items);
+      using (var db = new comessa5Entities())
+      {
+        //List<ItemViewModel> items = new List<ItemViewModel>();
+        List<ItemViewModel> items = await (from citem item 
+                                   in db.citem
+                                   where item.providerId == providerID
+                                   select new ItemViewModel
+                                   {
+                                     ID = item.id,
+                                     Name = item.name,
+                                     Price = item.price
+                                   }).ToListAsync();
+        return PartialView("ItemsView", items);
+      }
     }
+
+    /*[HttpGet]
+public async Task<ActionResult> GetCategoryProducts(string categoryId)  
+{
+    var lookupId = int.Parse(categoryId);
+    var model = await this.GetFullAndPartialViewModel(lookupId);
+    return PartialView("CategoryResults", model);
+}*/
   }
 }
