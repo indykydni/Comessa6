2482ed6219a396089e4c22845ef2c3d62b1f3399
michalguzek <michal.guzek@gmail.com>
2016-06-22 21:57:10 +0200

    the new create order partial view...

--- Comessa6/Comessa6/Controllers/OrdersController.cs
+++ Comessa6/Comessa6/Controllers/OrdersController.cs
@@ -10,9 +10,17 @@ namespace Comessa6.Controllers
     public class OrdersController : Controller
     {
         // GET: Orders
+        [HttpGet]
         public ActionResult CreateOrder(int itemID)
         {
           return PartialView("CreateOrderView", new OrderViewModel { ItemID = itemID, Quantity = 1 });
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int ID)
+        {
+            // Delete the item in the database
+            return Json(true); // or if there is an error, return Json(null); to indicate failure
         }
     }
 }
--- Comessa6/Comessa6/Scripts/Ordering.js
+++ Comessa6/Comessa6/Scripts/Ordering.js
@@ -1,15 +1,33 @@
 ﻿function ItemClicked(button) {
-    var orderItem = document.getElementById("orderItem");
-    document.getElementById("orderItem").value = button.getAttribute("itemName");
-    orderItem.setAttribute("itemID", button.id);
-    document.getElementById("orderQuantity").value = 1;
-    $("#orderPanel").collapse("show");
+    //var orderItem = document.getElementById("orderItem");
+    //document.getElementById("orderItem").value = button.getAttribute("itemName");
+    //orderItem.setAttribute("itemID", button.id);
+    //document.getElementById("orderQuantity").value = 1;
+    //$("#orderPanel").collapse("show");
+
+    var shownPanel = $("#orderPanel");
+    if (shownPanel)
+        shownPanel.remove();
+    var itemID = button.getAttribute("id");
+    var orderPanel = $("#panel" + itemID);
+    orderPanel.html(itemID);
+    $.ajax({
+        url: "Orders/CreateOrder",
+        type: 'GET',
+        cache: false,
+        async: false,        
+        data: { itemID },
+        success: function (result) { orderPanel.html(result); },
+        error: function (error) { alert("Database failure: " + error.statusText); }
+    });
+    return false;            
 }
 
 function CancelOrder() {
-    document.getElementById("orderItem").value = "";
-    document.getElementById("orderQuantity").value = "";
-    document.getElementById("orderComments").value = "";
-    $("#orderPanel").collapse("hide");
-
+    //document.getElementById("orderItem").value = "";
+    //document.getElementById("orderQuantity").value = "";
+    //document.getElementById("orderComments").value = "";
+    //$("#orderPanel").collapse("hide");
+    
+    $("#orderPanel").remove();
 }
