11eb0f647a25838340ddda17a9dbbcf510f75e27
michalguzek <michal.guzek@gmail.com>
2017-01-15 12:28:48 +0100

    tests

---
+++ Comessa6/Comessa6.Tests/Controllers/OrdersController/OrdersController_CreateOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Comessa6.Controllers;
+using System.Web.Mvc;
+
+namespace Comessa6.Tests.Controllers
+{
+  [TestFixture]
+  public class OrdersController_CreateOrder
+  {
+    [Test]
+    public void OrdersController_CreateOrder_ReturnsCorrectView()
+    {
+      var controller = new OrdersController();
+      var result = controller.CreateOrder(2) as PartialViewResult;
+      Assert.IsNotNull(result);
+      Assert.AreEqual(result.ViewName, "CreateOrderView");
+    }
+  }
+}
---
+++ Comessa6/Comessa6.TestsDebug/Program.cs
@@ -0,0 +1,18 @@
+﻿using Comessa6.Tests.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comessa6.TestsDebug
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var test = new OrdersController_CreateOrder();
+      test.ReturnsCorrectView();
+    }
+  }
+}
